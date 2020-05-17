757fa3a282573b65db3a8dfd1a63a5d27e425270
tral909 <indorm1992@mail.ru>
2020-05-17 22:57:43 +0300

    Create api client

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
-console.log('hello');
+class SwapiService {
+
+    _apiBase = 'https://swapi.dev/api';
+
+    async getResourse(url)  {
+        const response = await fetch(`${this._apiBase}${url}`);
+    
+        if (!response.ok) {
+            throw new Error(`Could not fetch ${url}, recieved ${response.status}`);
+        }
+        
+        const body = await response.json();
+        return body;
+    };
+
+    async getAllPeople()  {
+        const res = await this.getResourse(`/people`);
+        return res.results;
+    };
+
+    getPerson(id)  {
+        return this.getResourse(`/people/${id}`);
+    };
+
+    async getAllPlanets()  {
+        const res = await this.getResourse(`/planets`);
+        return res.results;
+    };
+
+    getPlanet(id)  {
+        return this.getResourse(`/planets/${id}`);
+    };
+
+    async getAllStarships()  {
+        const res = await this.getResourse(`/starships`);
+        return res.results;
+    };
+
+    getStarship(id)  {
+        return this.getResourse(`/starships/${id}`);
+    };
+
+}
+
+
+
+const swapi = new SwapiService();
+
+swapi.getPerson(3).then((p) => {
+    console.log(p.name);
+});
