8669076b5339aecd5d5e58d90d5c0b839e04313a
tral909 <indorm1992@mail.ru>
2020-05-18 09:13:16 +0300

    Add components and css

--- src/index.js
+++ src/index.js
@@ -1,51 +1,14 @@
-class SwapiService {
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-    _apiBase = 'https://swapi.dev/api';
-
-    async getResourse(url)  {
-        const response = await fetch(`${this._apiBase}${url}`);
-    
-        if (!response.ok) {
-            throw new Error(`Could not fetch ${url}, recieved ${response.status}`);
-        }
-        
-        const body = await response.json();
-        return body;
-    };
-
-    async getAllPeople()  {
-        const res = await this.getResourse(`/people`);
-        return res.results;
-    };
-
-    getPerson(id)  {
-        return this.getResourse(`/people/${id}`);
-    };
-
-    async getAllPlanets()  {
-        const res = await this.getResourse(`/planets`);
-        return res.results;
-    };
-
-    getPlanet(id)  {
-        return this.getResourse(`/planets/${id}`);
-    };
-
-    async getAllStarships()  {
-        const res = await this.getResourse(`/starships`);
-        return res.results;
-    };
-
-    getStarship(id)  {
-        return this.getResourse(`/starships/${id}`);
-    };
-
-}
-
-
+import SwapiService from './services/swapi-service';
 
 const swapi = new SwapiService();
 
 swapi.getPerson(3).then((p) => {
     console.log(p.name);
 });
+
+ReactDOM.render(<App />,
+    document.getElementById('root'));
