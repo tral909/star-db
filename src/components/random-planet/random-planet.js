9a4ae84354958d8854b45c3a345eb0be7e5aa568
tral909 <indorm1992@mail.ru>
2020-05-19 07:38:51 +0300

    Add network error handling in component

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import SwapiService from '../../services/swapi-service';
 import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator';
 
 import './random-planet.css';
 
@@ -22,26 +23,38 @@ export default class RandomPlanet extends Component {
   onPlanetLoaded = (planet) => {
     this.setState({
       planet,
-      loading: false
+      loading: false,
+      error: false
+    });
+  };
+
+  onError = (err) => {
+    this.setState({
+      error: true,
+      loading : false
     });
   };
 
   updatePlanet() {
-    const id = Math.floor(Math.random()) * 25 + 2;
+    const id = 12000;
     this.swapiService
       .getPlanet(id)
-      .then(this.onPlanetLoaded);
+      .then(this.onPlanetLoaded)
+      .catch(this.onError);
   }
 
   render() {
+    const { planet, loading, error } = this.state;
 
-    const { planet, loading } = this.state;
+    const hasData = !(loading || error);
 
+    const errorMessage = error ? <ErrorIndicator /> : null;
     const spinner = loading ? <Spinner /> : null;
-    const content = !loading ? <PlanetView planet={planet}/> : null;
+    const content = hasData ? <PlanetView planet={planet}/> : null; 
 
     return (
       <div className="random-planet jumbotron rounded">
+        {errorMessage}
         {spinner}
         {content}
       </div>
