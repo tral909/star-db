635f10462c254820864ada493615c7d94e073b96
tral909 <indorm1992@mail.ru>
2020-05-23 17:20:25 +0300

    Add react Links

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './header.css';
+import { Link } from 'react-router-dom';
 
 const Header = ({ onServiceChange }) => {
   return (
@@ -11,13 +12,13 @@ const Header = ({ onServiceChange }) => {
       </h3>
       <ul className="d-flex">
         <li>
-          <a href="#">People</a>
+          <Link to="/people">People</Link>
         </li>
         <li>
-          <a href="#">Planets</a>
+          <Link to="/planets">Planets</Link>
         </li>
         <li>
-          <a href="#">Starships</a>
+          <Link to="/starships">Starships</Link>
         </li>
       </ul>
 
